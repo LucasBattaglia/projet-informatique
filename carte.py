83acf9ee0a5f46d9526b5e2417ad4fe46e306c4b
Lucas <Lucas.battaglia@free.fr>
2022-02-01 17:37:27 +0100

    creation fonction carte_to_chaine

--- carte.py
+++ carte.py
@@ -1,1 +1,16 @@
-#fichier.py
+def carte_to_chaine(dic_carte):
+    if dic_carte['couleur']=='P':
+        couleur = chr(9824)
+    elif dic_carte['couleur']=='C':
+        couleur = chr(9825)
+    elif dic_carte['couleur']=='K':
+        couleur = chr(9826)
+    elif dic_carte['couleur']=='T':
+        couleur = chr(9827)
+    if dic_carte['valeur']>10:
+        return "{}{}".format(dic_carte['valeur'],couleur)
+    else:
+        return " {}{}".format(dic_carte['valeur'],couleur)
+
+
+print(carte_to_chaine({'valeur':'R', 'couleur':'P'}))
